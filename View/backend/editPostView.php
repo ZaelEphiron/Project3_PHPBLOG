c3e5f6bf9d1f12c0d9963dd4c90ad4b52166cad3
ZaelEphiron <michael.chassaing@outlook.fr>
2018-08-03 14:23:14 +0200

    Added french lang on tinymce interface

--- View/backend/editPostView.php
+++ View/backend/editPostView.php
@@ -33,7 +33,8 @@
 <script src="Public/assets/js/tinymce/tinymce.min.js"></script>
 <script>
   tinymce.init({
-    selector: '.tinymce'
+    selector: '.tinymce',
+    language: 'fr_FR'
   });
 </script>
 
